69e8be1abb1b385b986d2c66ffa584c9c47dd656
Miguel Padilla <mpadi21@mit.edu>
2022-07-07 18:03:12 -0600

    Initialized file structure.

---
+++ img/metrics.go
@@ -0,0 +1,53 @@
+package img
+
+func Mse(reference, subject imageMatrix) (float64, error) {
+	return nil
+}
+
+func Rmse(reference, subject imageMatrix) (float64, error) {
+	return nil
+}
+
+func Sam(reference, subject imageMatrix) (float64, error) {
+	return nil
+}
+
+func Race(reference, subject imageMatrix) (float64, error) {
+	return nil
+}
+
+func Ergas(reference, subject imageMatrix) (float64, error) {
+	return nil
+}
+
+func Uqi(reference, subject imageMatrix) (float64, error) {
+	return nil
+}
+
+func Ssim(reference, subject imageMatrix) (float64, error) {
+	return nil
+}
+
+func Psnr(reference, subject imageMatrix) (float64, error) {
+	return nil
+}
+
+func Msssimi(reference, subject imageMatrix) (float64, error) {
+	return nil
+}
+
+func Vif(reference, subject imageMatrix) (float64, error) {
+	return nil
+}
+
+func Dlambda(reference, subject imageMatrix) (float64, error) {
+	return nil
+}
+
+func Ds(reference, subject imageMatrix) (float64, error) {
+	return nil
+}
+
+func Qnr(reference, subject imageMatrix) (float64, error) {
+	return nil
+}
